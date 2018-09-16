0f079fdbed22908d4a4622310f9f3b12b56eff75
Nikolay Seleznev <nnseleznev@gmail.com>
2018-09-16 23:17:01 +0300

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,21 @@
+import pyautogui
+import random
+import time
+
+print('Press Ctrl-C to quit.')
+
+delay = 5 #Time of delay between movements
+screenX, screenY = pyautogui.size() #Define screen resolution
+monitorCount = 1 #Define monitors quantity
+
+try:
+    #Fall in to loop. Each iteration is a new movement
+    while True:
+        #Define endpoint of movement
+        endPositionX, endPositionY = (random.randint(0, screenX) * monitorCount, random.randint(0, screenY))
+        #Move mouse to coordinates
+        pyautogui.moveTo(endPositionX, endPositionY, 3, pyautogui.easeInOutQuad)
+        #Wait for one second after movement
+        time.sleep(delay)
+except KeyboardInterrupt:
+    print('\n')
