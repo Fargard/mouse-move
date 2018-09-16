0edf68f63d01db6cebde6834891f5c9c1ee7a33d
Nikolay Seleznev <nnseleznev@gmail.com>
2018-09-16 23:20:25 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ import time
 
 print('Press Ctrl-C to quit.')
 
-delay = 5 #Time of delay between movements
+delay = 5 #Time of delay in seconds between movements
 screenX, screenY = pyautogui.size() #Define screen resolution
 monitorCount = 1 #Define monitors quantity
 
@@ -15,7 +15,7 @@ try:
         endPositionX, endPositionY = (random.randint(0, screenX) * monitorCount, random.randint(0, screenY))
         #Move mouse to coordinates
         pyautogui.moveTo(endPositionX, endPositionY, 3, pyautogui.easeInOutQuad)
-        #Wait for one second after movement
+        #Wait for delay after movement
         time.sleep(delay)
 except KeyboardInterrupt:
     print('\n')
